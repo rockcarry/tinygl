55c40d88cdabe7f959fc281164036967b0033ca2
ck <rockcarry@163.com>
2022-10-08 13:32:17 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-gcc --static -Wall -Ofast -flto -D_TEST_TEXTURE_ -o test_texture texture.c
-gcc --static -Wall -Ofast -flto -D_TEST_MODEL_   -o test_model   texture.c model.c
-gcc --static -Wall -Ofast -flto -D_TEST_WINGDI_  -o test_wingdi  wingdi.c -lgdi32
-gcc --static -Wall -Ofast -flto -D_TEST_TINYGL_  -o test_tinygl  utils.c vector.c matrix.c texture.c triangle.c model.c shader.c wingdi.c tinygl.c -lgdi32
-gcc --static -Wall -Ofast -flto -o tinygl_demo utils.c vector.c matrix.c texture.c triangle.c model.c shader.c wingdi.c tinygl.c demo.c -lgdi32
+gcc --static -Wall -march=native -Ofast -flto -D_TEST_TEXTURE_ -o test_texture texture.c
+gcc --static -Wall -march=native -Ofast -flto -D_TEST_MODEL_   -o test_model   texture.c model.c
+gcc --static -Wall -march=native -Ofast -flto -D_TEST_WINGDI_  -o test_wingdi  wingdi.c -lgdi32
+gcc --static -Wall -march=native -Ofast -flto -D_TEST_TINYGL_  -o test_tinygl  utils.c vector.c matrix.c texture.c triangle.c model.c shader.c wingdi.c tinygl.c -lgdi32
+gcc --static -Wall -march=native -Ofast -flto -o tinygl_demo utils.c vector.c matrix.c texture.c triangle.c model.c shader.c wingdi.c tinygl.c demo.c -lgdi32
 strip --strip-all *.exe
 
